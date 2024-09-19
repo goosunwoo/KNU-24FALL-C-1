5361b5d60c26639764e0bd27116b0f47e6fca719
whdtjd <cjs05david@gmail.com>
2024-09-19 14:41:55 +0900

    재귀함수 실습

--- day 03/day03-02.c
+++ day 03/day03-02.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-int factorial(a)
+int factorial(int a)
 {
-	int num, num1;
-	for (num = 1; num < a; num++)
-	{
-		a = a * num;
-		return a;
-	}
+	int num;
+	if (a == 0 || a == 1)
+		return 1;
+	num = a * factorial(a - 1);
 }
 int main()
 {
 	int a, b;
 	printf("계산할 팩토리얼 값 입력: ");
 	scanf_s("%d", &a);
-	
-	printf("a! = %d", factorial(a));
+	factorial(a);
+	printf("%d! = %d", a, factorial(a));
+	return 0;
 }
